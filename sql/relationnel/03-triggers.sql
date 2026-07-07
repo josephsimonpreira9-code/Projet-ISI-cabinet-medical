6143d6326d964ee453079331c9e33d54a03f9983
mara02soukeye-ui <mara02soukeye@gmail.com>
2026-07-07 15:28:55 +0200

    Ajout trigger contrainte au moins un médicament par ordonnance

--- sql/relationnel/03-triggers.sql
+++ sql/relationnel/03-triggers.sql
@@ -1,1 +1,19 @@
+DELIMITER //
 
+CREATE TRIGGER empecher_ordonnance_vide
+BEFORE DELETE ON PRESCRIT
+FOR EACH ROW
+BEGIN
+    DECLARE nb_medicaments INT;
+
+    SELECT COUNT(*) INTO nb_medicaments
+    FROM PRESCRIT
+    WHERE NUM_ORDONNANCE = OLD.NUM_ORDONNANCE;
+
+    IF nb_medicaments <= 1 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Impossible de supprimer : une ordonnance doit contenir au moins un médicament.';
+    END IF;
+END //
+
+DELIMITER ;
